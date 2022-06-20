099abeeb7a68fa8dc63a30c4335404d545b1fd39
Yan NOW <tumakaev.jan@yandex.ru>
2022-06-20 18:59:14 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/TextFragment.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class TextFragment extends Fragment {
+
+
+
+
+    public static TextFragment newInstance() {
+        TextFragment fragment = new TextFragment();
+        return fragment;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_texst, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/TextNameFragment.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class TextNameFragment extends Fragment {
+
+
+
+
+
+
+    public static TextNameFragment newInstance() {
+        TextNameFragment fragment = new TextNameFragment();
+        return fragment;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_texst_name, container, false);
+    }
+}
